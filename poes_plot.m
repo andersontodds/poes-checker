e8d0e341c2b6d1e0cd9acd404ef9e7d96ea83abd
todd <andersontodds@gmail.com>
2022-10-21 17:14:54 -0700

    move data import to function

--- poes_plot.m
+++ poes_plot.m
@@ -18,61 +18,67 @@
 %   NOAA-18
 %   NOAA-19
 
-% display contents of single file
-n18_file = "data/poes_n18_20221018_proc.nc";
-n19_file = "data/poes_n19_20221018_proc.nc";
-%ncdisp(poesfile);
+% % display contents of single file
+% n18_file = "data/poes_n18_20221018_proc.nc";
+% n19_file = "data/poes_n19_20221018_proc.nc";
+% ncdisp(poesfile);
 
-% time
-n18_time_ms = cast(ncread(n18_file, "time"), "double"); % milliseconds since January 1, 1970 00:00 UTC
-n18_time_dn = n18_time_ms/(1000*60*60*24) + datenum(1970,01,01);
-n18_time_dt = datetime(n18_time_dn, "ConvertFrom", "datenum");
+datafields = ["time"; "lat"; "lon"; "alt"; "geod_lat_foot"; "geod_lon_foot"; ...
+    "mep_ele_tel0_flux_e2"; "mep_ele_tel0_flux_e3" ;"mep_ele_tel0_flux_e4"; ...
+    "mep_ele_tel90_flux_e2"; "mep_ele_tel90_flux_e3"; "mep_ele_tel90_flux_e4"];
 
-n19_time_ms = cast(ncread(n19_file, "time"), "double"); % milliseconds since January 1, 1970 00:00 UTC
-n19_time_dn = n19_time_ms/(1000*60*60*24) + datenum(1970,01,01);
-n19_time_dt = datetime(n19_time_dn, "ConvertFrom", "datenum");
+n18 = poesimport(2022,10,18,"n18",datafields);
+n19 = poesimport(2022,10,18,"n19",datafields);
 
-% latitude, longitude, altitude
-n19_lat = cast(ncread(n19_file, "lat"), "double");      % degrees
-n19_lon = cast(ncread(n19_file, "lon"), "double");      % degrees    
-n19_alt = cast(ncread(n19_file, "alt"), "double");      % km
-
-% magnetic field/magnetic coordinates
-% POES data come with many different magnetic field components and
-% coordinates, including coordinates of the magnetic foot point, magnetic
-% field components at both the satellite and satellite foot point, and the
-% familiar L-shell and MLT.  For now, let's only import the geodetic
-% coordinates of the footpoint.
-n19_foot_lat = cast(ncread(n19_file, "geod_lat_foot"), "double");
-n19_foot_lon = cast(ncread(n19_file, "geod_lon_foot"), "double");
-
-% electron channels
-% MEPED has two electron telescopes, oriented approximately 0 degrees and
-% 90 degrees to the local vertical.  Each telescope has a 13-degree field
-% of view. At middle and high latitudes, the 0 degree telescope measures
-% precipitating flux, and the 90 degree telescope measures trapped flux.
-%
-% TODO: check electron energy levels: documentation in PDF does not match
-% NC file channel descriptions
-% Each telescope measures electrons in three energy bands:
-%   E1:  30 keV to 2500 keV
-%   E2: 100 keV to 2500 keV
-%   E3: 300 keV to 2500 keV
-% Flux of lower-energy electrons (e.g. 30-100 keV) can be obtained by
-% subtracting the E2 flux from the E1 flux.  We are mainly concerned with
-% electrons in the 100 keV-1 MeV range, and will be using these data
-% qualitatively, so the unmodified E2 and E3 channels should suffice.
-
-% all electron fluxes have units of (counts cm^-2 sr^-1 s^-1)
-
-n19_tel0_flux_e2 = cast(ncread(n19_file, "mep_ele_tel0_flux_e2"), "double");
-n19_tel0_flux_e3 = cast(ncread(n19_file, "mep_ele_tel0_flux_e3"), "double");
-n19_tel0_flux_e4 = cast(ncread(n19_file, "mep_ele_tel0_flux_e4"), "double");
-
-n19_tel90_flux_e2 = cast(ncread(n19_file, "mep_ele_tel90_flux_e2"), "double");
-n19_tel90_flux_e3 = cast(ncread(n19_file, "mep_ele_tel90_flux_e3"), "double");
-n19_tel90_flux_e4 = cast(ncread(n19_file, "mep_ele_tel90_flux_e4"), "double");
-
+% % time
+% n18_time_ms = cast(ncread(n18_file, "time"), "double"); % milliseconds since January 1, 1970 00:00 UTC
+% n18_time_dn = n18_time_ms/(1000*60*60*24) + datenum(1970,01,01);
+% n18_time_dt = datetime(n18_time_dn, "ConvertFrom", "datenum");
+% 
+% n19_time_ms = cast(ncread(n19_file, "time"), "double"); % milliseconds since January 1, 1970 00:00 UTC
+% n19_time_dn = n19_time_ms/(1000*60*60*24) + datenum(1970,01,01);
+% n19_time_dt = datetime(n19_time_dn, "ConvertFrom", "datenum");
+% 
+% % latitude, longitude, altitude
+% n19_lat = cast(ncread(n19_file, "lat"), "double");      % degrees
+% n19_lon = cast(ncread(n19_file, "lon"), "double");      % degrees    
+% n19_alt = cast(ncread(n19_file, "alt"), "double");      % km
+% 
+% % magnetic field/magnetic coordinates
+% % POES data come with many different magnetic field components and
+% % coordinates, including coordinates of the magnetic foot point, magnetic
+% % field components at both the satellite and satellite foot point, and the
+% % familiar L-shell and MLT.  For now, let's only import the geodetic
+% % coordinates of the footpoint.
+% n19_foot_lat = cast(ncread(n19_file, "geod_lat_foot"), "double");
+% n19_foot_lon = cast(ncread(n19_file, "geod_lon_foot"), "double");
+% 
+% % electron channels
+% % MEPED has two electron telescopes, oriented approximately 0 degrees and
+% % 90 degrees to the local vertical.  Each telescope has a 13-degree field
+% % of view. At middle and high latitudes, the 0 degree telescope measures
+% % precipitating flux, and the 90 degree telescope measures trapped flux.
+% %
+% % TODO: check electron energy levels: documentation in PDF does not match
+% % NC file channel descriptions
+% % Each telescope measures electrons in three energy bands:
+% %   E1:  30 keV to 2500 keV
+% %   E2: 100 keV to 2500 keV
+% %   E3: 300 keV to 2500 keV
+% % Flux of lower-energy electrons (e.g. 30-100 keV) can be obtained by
+% % subtracting the E2 flux from the E1 flux.  We are mainly concerned with
+% % electrons in the 100 keV-1 MeV range, and will be using these data
+% % qualitatively, so the unmodified E2 and E3 channels should suffice.
+% 
+% % all electron fluxes have units of (counts cm^-2 sr^-1 s^-1)
+% 
+% n19_tel0_flux_e2 = cast(ncread(n19_file, "mep_ele_tel0_flux_e2"), "double");
+% n19_tel0_flux_e3 = cast(ncread(n19_file, "mep_ele_tel0_flux_e3"), "double");
+% n19_tel0_flux_e4 = cast(ncread(n19_file, "mep_ele_tel0_flux_e4"), "double");
+% 
+% n19_tel90_flux_e2 = cast(ncread(n19_file, "mep_ele_tel90_flux_e2"), "double");
+% n19_tel90_flux_e3 = cast(ncread(n19_file, "mep_ele_tel90_flux_e3"), "double");
+% n19_tel90_flux_e4 = cast(ncread(n19_file, "mep_ele_tel90_flux_e4"), "double");
 
 %% plot electron flux on each channel as a function of time
 figure(1)
@@ -84,10 +90,10 @@ colors = colors(2:end, :);
 tiledlayout(2,1,"TileSpacing","compact","Padding","compact");
 ax1 = nexttile;
 hold off
-semilogy(n19_time_dt, n19_tel0_flux_e2, '.');
+semilogy(datetime(n18.time,"ConvertFrom", "datenum"), n18.mep_ele_tel0_flux_e2, '.');
 hold on
-semilogy(n19_time_dt, n19_tel0_flux_e3, '.');
-semilogy(n19_time_dt, n19_tel0_flux_e4, '.');
+semilogy(datetime(n18.time,"ConvertFrom", "datenum"), n18.mep_ele_tel0_flux_e3, '.');
+semilogy(datetime(n18.time,"ConvertFrom", "datenum"), n18.mep_ele_tel0_flux_e4, '.');
 ylim([1E2 1E6]);
 colororder(ax1, colors);
 legend("E2", "E3", "E4");
@@ -95,10 +101,10 @@ title("MEPED 0\circ telescope");
 
 ax2 = nexttile;
 hold off
-semilogy(n19_time_dt, n19_tel90_flux_e2, '.');
+semilogy(datetime(n18.time,"ConvertFrom", "datenum"), n18.mep_ele_tel90_flux_e2, '.');
 hold on
-semilogy(n19_time_dt, n19_tel90_flux_e3, '.');
-semilogy(n19_time_dt, n19_tel90_flux_e4, '.');
+semilogy(datetime(n18.time,"ConvertFrom", "datenum"), n18.mep_ele_tel90_flux_e3, '.');
+semilogy(datetime(n18.time,"ConvertFrom", "datenum"), n18.mep_ele_tel90_flux_e4, '.');
 ylim([1E2 1E6]);
 colororder(ax2, colors);
 legend("E2", "E3", "E4");
@@ -114,8 +120,8 @@ hold off
 worldmap('World');
 geoshow(coastlat, coastlon, "Color", "black");
 hold on
-scatterm(n19_lat, n19_lon, 0.5, [0.5 0.5 0.5]);
-scatterm(n19_lat, n19_lon, 5, n19_tel0_flux_e2);
+scatterm(n18.lat, n18.lon, 0.5, [0.5 0.5 0.5]);
+scatterm(n18.lat, n18.lon, 5, n18.mep_ele_tel0_flux_e2);
 % xlabel("Latitude");
 % ylabel("Longitude");
 t1 = title("MEPED 0\circ telescope E2 flux");
@@ -130,8 +136,8 @@ hold off
 worldmap('World');
 geoshow(coastlat, coastlon, "Color", "black");
 hold on
-scatterm(n19_lat, n19_lon, 0.5, [0.5 0.5 0.5]);
-scatterm(n19_lat, n19_lon, 5, n19_tel90_flux_e2);
+scatterm(n18.lat, n18.lon, 0.5, [0.5 0.5 0.5]);
+scatterm(n18.lat, n18.lon, 5, n18.mep_ele_tel90_flux_e2);
 % xlabel("Latitude");
 % ylabel("Longitude");
 t2 = title("MEPED 90\circ telescope E2 flux");
@@ -145,32 +151,3 @@ cb.Label.String = "counts cm^{-2} sr^{-1} s^{-1}";
 cb.Label.FontSize = 15;
 cb.FontSize = 12;
 caxis([10 100000]);
-
-
-%% function definitions
-
-function outstruct = poesimport(filename, varargin)
-% import specified data from POES NetCDF files
-% inputs:                           type    example
-%   filename:   NetCDF filename     string  "poes_n18_20221018_proc.nc"
-%   varargin:   field names         string  "time", "mep_ele_tel0_flux_e2"
-%
-% outputs:
-%   outstruct:  struct of data from NetCDF file
-%
-% usage:
-% filename = ["poes_n18_20221018_proc.nc"; "poes_n19_20221018_proc.nc"; "poes_m01_20221018_proc.nc"];
-% fields = ["time"; "lat"; "lon"; "alt"; ...
-%   "mep_ele_tel0_flux_e2"; "mep_ele_tel0_flux_e3"; "mep_ele_tel0_flux_e4"; ...
-%   "mep_ele_tel90_flux_e2"; "mep_ele_tel90_flux_e3"; "mep_ele_tel90_flux_e4"];
-% 
-% poesdata = poesimport(filename, fields)
-
-% TODO: 
-%   1. consider changing filename inputs to spacecraft and yyyy mm dd,
-%   like in poes-curl script
-%   2. eventually we want to look at files quickly and automatically
-
-
-
-end
---
+++ poesimport.m
@@ -0,0 +1,92 @@
+function out = poesimport(varargin)
+% Import specified data from POES NetCDF files.
+% 
+% INPUTS: a POES data file must be specified with either an exact filename,
+% or date and satellite identifier.  An example filename is:
+%   "poes_n18_20221018_proc.nc"
+% where       n18:        satellite identifier
+%             20221018:   date - yyyymmdd
+% Alternatively, the date and satellite IDs can be specified as four
+% inputs, e.g. 
+%   2022, 10, 18, "n18"
+% After specifying either the filename or the date and satellite, the
+% fields to be imported from the NetCDF file must be specified.  Fields can
+% be listed as individual char or string arguments, or in a single vector
+% of strings, e.g.:
+%   "time", "lat", "lon", ...
+% or
+%   fields = ["time"; "lat"; "lon; ...]
+%
+% OUTPUTS:
+%   out:  struct of data from NetCDF file
+%
+% Usage examples:
+% 1: filename and field list
+% filename = "poes_n18_20221018_proc.nc";
+% fields = ["time"; "lat"; "lon"; "alt"; ...
+%   "mep_ele_tel0_flux_e2"; "mep_ele_tel0_flux_e3"; "mep_ele_tel0_flux_e4"; ...
+%   "mep_ele_tel90_flux_e2"; "mep_ele_tel90_flux_e3"; "mep_ele_tel90_flux_e4"];
+% 
+% poesdata = poesimport(filename, fields);
+%
+% 2. date, satellite identifier, and individual fields
+% poesdata = poesimport(2022,10,18,"n18", "time","lat","lon");
+%
+
+% check whether first inputs are a filename, or yyyy mm dd sat.
+
+if ischar(varargin{1}) || isstring(varargin{1}) % first input is filename
+    file = string(varargin{1});
+    filename = sprintf("data/%s", file);
+    field_start_ind = 2;
+
+elseif varargin{1} > 2011 && varargin{1} < 2100 % first input is year
+    year = varargin{1};
+    month = varargin{2};
+    day = varargin{3};
+    sat = string(varargin{4});
+
+    switch sat
+        case {"n15", "noaa15"}
+            sat = "n15";
+        case {"n18", "noaa18"}
+            sat = "n18";
+        case {"n19", "noaa19"}
+            sat = "n19";
+        case {"m01", "metop01"}
+            sat = "m01";
+        case {"m03", "metop03"}
+            sat = "m03";
+        otherwise
+            error("Invalid satellite specifier! Options are n15, n18, n19, m01, m03.");
+    end
+    
+    filename = sprintf("data/poes_%s_%04d%02d%02d_proc.nc", sat, year, month, day);
+    field_start_ind = 5;
+else 
+    error("Invalid input! Specify either a filename or year, month, day and satellite ID.\nType 'help poesimport' for more info.");
+end
+
+% check whether fields to import are listed individually or in vector of
+% strings
+
+if length(string(varargin{field_start_ind})) == 1   % fields listed individually
+    fieldnames = string(varargin(field_start_ind:end));
+    fieldnames = fieldnames';
+else    % fields listed in single vector of strings
+    fieldnames = varargin{field_start_ind};
+end
+out = struct();
+
+% generate struct of outputs
+for i = 1:length(fieldnames)
+    fielddata = cast(ncread(filename, fieldnames{i}), "double");
+    % convert time to datenum format
+    if fieldnames{i} == "time"
+        fielddata = fielddata./(1000*60*60*24) + datenum(1970, 01, 01);
+    end
+    
+    out.(fieldnames{i}) = fielddata;
+end
+
+end
